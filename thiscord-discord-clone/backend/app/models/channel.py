523bd4eb39b51da272d21418e9ac7ab93f77e280
Haris Ahmed <iamharis08@gmail.com>
2022-12-09 17:13:14 -0500

    channel

--- thiscord-discord-clone/backend/app/models/channel.py
+++ thiscord-discord-clone/backend/app/models/channel.py
@@ -10,5 +10,4 @@ class Channels(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    server_id = db.Column(db.String(255), nullable=False, db.ForeignKey('servers.id'))
-   
+    server_id = db.Column(db.Integer, db.ForeignKey('servers.id'), nullable=False,)
