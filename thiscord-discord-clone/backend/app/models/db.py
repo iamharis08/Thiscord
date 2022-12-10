275622174903a3d740982b485023de94c8d4d6e0
Trevor WM <Trevormoore.2016@gmail.com>
2022-12-09 23:11:16 -0800

    deployment debugging

--- thiscord-discord-clone/backend/app/models/db.py
+++ thiscord-discord-clone/backend/app/models/db.py
@@ -8,23 +8,23 @@ SCHEMA = os.environ.get("SCHEMA")
 
 db = SQLAlchemy()
 
-servers_users = db.Table(
-    "servers_users",
-    db.Model.metadata,
-    db.Column(
-        "server_id", db.Integer, db.ForeignKey("servers.id"), primary_key=True
-    ),
-    db.Column(
-        "user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True
-    )
-)
-
-if environment == "production":
-    servers_users.schema = SCHEMA
-
 # helper function for adding prefix to foreign key column references in production
 def add_prefix_for_prod(attr):
     if environment == "production":
         return f"{SCHEMA}.{attr}"
     else:
         return attr
+
+servers_users = db.Table(
+    "servers_users",
+    db.Model.metadata,
+    db.Column(
+        "server_id", db.Integer, db.ForeignKey(add_prefix_for_prod("servers.id")), primary_key=True
+    ),
+    db.Column(
+        "user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True
+    )
+)
+
+if environment == "production":
+    servers_users.schema = SCHEMA
