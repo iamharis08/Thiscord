f168853bcc499c8d8a104b0475abae5726025802
Jacob Lauxman <jlauxman@gmail.com>
2022-12-11 19:19:56 -0700

    issues with running flask command

--- thiscord-discord-clone/frontend/react-app/src/components/Channel.js
+++ thiscord-discord-clone/frontend/react-app/src/components/Channel.js
@@ -2,56 +2,55 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Channel() {
-  const [server, setServer] = useState({});
-  const [users, setUsers] = useState([]);
-  const [channels, setChannels] = useState([])
-  const { serverId }  = useParams();
+  const [channel, setChannel] = useState({})
+  const [messages, setMessages] = useState([]);
+  const { channelId } = useParams();
 
   useEffect(() => {
-    if (!serverId) {
+    if (!channelId) {
       return;
     }
     (async () => {
-      const response = await fetch(`/api/servers/${serverId}`);
+      const response = await fetch(`/api/channels/${channelId}`);
       const responseData = await response.json();
-      setServer(responseData.server);
-    //   console.log(responseData.users, 'RESPONSE USER ')
-      setUsers(responseData.users)
-      setChannels(responseData.channels)
+      setChannel(responseData.channel);
+      //   console.log(responseData.users, 'RESPONSE USER ')
+      setMessages(responseData.messages)
+      // setChannels(responseData.channels)
     })();
-  }, [serverId]);
+  }, [channelId]);
 
-  if (!server) {
+  if (!channel) {
     return null;
   }
 
   return (
     <>
-    <ul>
-      <li>
-        <strong>Server Id</strong> {serverId}
-      </li>
-      <li>
-        <strong>Server Name</strong> {server.name}
-      </li>
-        </ul>
-      <div>
-        <strong>Users</strong>
-        {users?.map(user => (
-          <li key={user?.id}>
-          {user?.username}
+      <ul>
+        <li>
+          <strong>Channel Id</strong> {channelId}
         </li>
+        <li>
+          <strong>Channel Name</strong> {channel.name}
+        </li>
+      </ul>
+      <div>
+        <strong>Messages</strong>
+        {messages?.map(message => (
+          <li key={message?.id}>
+            {message?.message}
+          </li>
         ))}
       </div>
-<div>
+      {/* <div>
 <strong>Channels</strong>
         {channels?.map(channel => (
           <li key={channel?.id}>
           {channel?.name}
             </li>
         ))}
-</div>
-</>
+</div> */}
+    </>
   );
 }
 export default Channel;
