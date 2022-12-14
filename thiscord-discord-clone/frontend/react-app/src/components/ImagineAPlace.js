1e8a236175c4947744c80cd608b820f72235844f
tyler <infinitespace20@gmail.com>
2022-12-13 22:44:30 -0600

    finished 3 sections on splash page

--- thiscord-discord-clone/frontend/react-app/src/components/ImagineAPlace.js
+++ thiscord-discord-clone/frontend/react-app/src/components/ImagineAPlace.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../css/ImagineAPlace.css'
 import channelsHomeSvg from '../css/images/thiscord-channel-img.svg'
+import voiceChannelImg from '../css/images/voice-channel-img.svg'
+import membersHomeImg from '../css/images/members-home-img.svg'
+import voiceCallImg from '../css/images/voice-call-img.svg'
 
 
 const ImagineAPlace = () => {
@@ -20,13 +23,45 @@ const ImagineAPlace = () => {
                     <img src={channelsHomeSvg} alt='something in there' />
                 </div>
                 <div className='channels-home-text'>
-                    <h3 id='channels-home-h3'>Create an invite-only place where you belong</h3>
+                    <h2 id='channels-home-h2'>Create an invite-only place where you belong</h2>
                     <span className='channels-home-body-text'>
                     Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.
                     </span>
                 </div>
             </div>
-
+            <div className='voice-channels-section-container'>
+                <div className='voice-channels-home-text'>
+                    <h2 id='voice-channels-home-h2'>Where hanging out is easy</h2>
+                    <span className='voice-channels-home-body-text'>
+                    Grab a seat in a voice channel when you're free. Friends in your server can see you're around and instantly pop in to talk without having to call.
+                    </span>
+                </div>
+                <div className='voice-channels-home-image'>
+                    <img src={voiceChannelImg} alt=''/>
+                </div>
+            </div>
+            <div className='members-section-container'>
+                <div className='members-home-image'>
+                    <img src={membersHomeImg} alt=''/>
+                </div>
+                <div className='members-home-text'>
+                    <h2 id='members-home-h2'>From few to a fandom</h2>
+                    <span className='members-home-body-text'>
+                    Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.
+                    </span>
+                </div>
+            </div>
+            <div className='voice-call-section-container'>
+                <div className='voice-call-home-text'>
+                    <h2 id='voice-call-home-h2'>RELIABLE TECH FOR STAYING CLOSE</h2>
+                    <span className='voice-call-home-body-text'>
+                    Low-latency voice and video feels like you're in the same room. Wave hello over video, watch friends stream their games, or gather up and have a drawing session with screen share.
+                    </span>
+                </div>
+                <div className='voice-call-home-image'>
+                    <img src={voiceCallImg} alt=''/>
+                </div>
+            </div>
         </main>
     )
 }
