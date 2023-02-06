835dc1b059c39058e134b124c950b07d84b9743c
Haris Ahmed <iamharis08@gmail.com>
2023-02-05 22:36:51 -0500

    migration render schema

--- thiscord-discord-clone/backend/migrations/versions/20230204_163939_added_live_id_in_messages_model.py
+++ thiscord-discord-clone/backend/migrations/versions/20230204_163939_added_live_id_in_messages_model.py
@@ -1,13 +1,16 @@
 """added live id in messages model
 
 Revision ID: c8f68c5ef625
-Revises: 
+Revises:
 Create Date: 2023-02-04 16:39:39.938762
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'c8f68c5ef625'
@@ -61,6 +64,23 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    # ### end Alembic commands ###
+
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE servers SET SCHEMA {SCHEMA};")
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE channels SET SCHEMA {SCHEMA};")
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE servers_users SET SCHEMA {SCHEMA};")
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE messages SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
